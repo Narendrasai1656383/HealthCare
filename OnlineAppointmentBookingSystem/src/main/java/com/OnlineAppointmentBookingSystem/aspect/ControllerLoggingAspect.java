c6da783f71957224dc2564e81b2bc365e7ac2115
Narendra Sai Rangisetty <155012486+Narendrasai1656383@users.noreply.github.com>
2025-04-09 23:40:22 +0530

    Create ControllerLoggingAspect.java
    commit

---
+++ OnlineAppointmentBookingSystem/src/main/java/com/OnlineAppointmentBookingSystem/aspect/ControllerLoggingAspect.java
@@ -0,0 +1,33 @@
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ControllerLoggingAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(ControllerLoggingAspect.class);
+
+    @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
+    public void restControllerMethods() {}
+
+    @Around("restControllerMethods()")
+    public Object logRequestAndResponse(ProceedingJoinPoint joinPoint) throws Throwable {
+        String method = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+
+        log.info("Incoming request to {} with args: {}", method, args);
+
+        Object result = joinPoint.proceed();
+
+        log.info("Response from {} with result: {}", method, result);
+
+        return result;
+    }
+}
